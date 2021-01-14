5fdbf094ca2cd1cba28f288235ba228e0afc2c84
NightOwl <midnightow1@outlook.com>
2021-01-14 17:59:12 +0800

    Implemented python yolo inference

--- Control_Simulator/main.py
+++ Control_Simulator/main.py
@@ -1,12 +1,25 @@
 import cv2
-import sys
+import argparse
+
+from sklearn.metrics._plot import det_curve
+
 from yolo_detect import YoloDetector
 
 def onDetected(bboxes):
     """
     Analyze bounding boxes and infer the target position
     Prototyping skiing robot control algorithm here
-    :param bboxes: bounding boxes array; each bounding box element is a (xmin, ymin, xmax, ymax) tuple
+    :param bboxes: bounding boxes torch tensor;
+        Example tensor:
+            [tensor([
+                    [142.25000,  98.31250, 148.25000, 107.93750,   0.50928,   0.00000],
+                    [107.56250,  94.00000, 112.68750, 102.75000,   0.42310,   0.00000]
+                ], device='cuda:0')]
+        Example code:
+            for i, det in enumerate(pred):  # detections per image
+                if len(det):
+                    for *xyxy, conf, cls in reversed(det):
+
     :return: a (x, y) tuple; indicates the expected target position in the given frame image;
     """
 
@@ -21,19 +34,49 @@ def onDetected(bboxes):
 # https://www.youtube.com/watch?v=M4XbJqRO00s
 # https://www.youtube.com/watch?v=JsJY9U74l7w
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yolov5-root', type=str, default='./yolov5', help='Path to yolov5 root directory')
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='test_video/vid0.MOV', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=224, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    opt = parser.parse_args()
+    print(opt)
+
     cap = cv2.VideoCapture("test_video/vid0.MOV")
     window_name = "Skiing Control Simulator    -    Press \"Q\" to exit"
 
-    detector = YoloDetector()
-
+    detector = YoloDetector(opt.weights, opt.device, opt.img_size, opt.conf_thres, opt.iou_thres)
     while True:
         ret, frame = cap.read()
-        # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # rgb = cv2.resize(rgb, 224, 224)
 
-        # TODO: yolov5 inference 
-        # result = model(rgb)
-        # onDetected(result)
+        result = detector.Detect(frame)
+
+        # Visualize detection results
+        line_thickness = round(0.002 * (frame.shape[0] + frame.shape[1]) / 2) + 1
+        font_thickness = max(line_thickness - 1, 1)
+        for i, det in enumerate(result):  # detections per image
+            if len(det):
+                for *xyxy, conf, cls in reversed(det):
+                    xmin, ymin, xmax, ymax = (int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3]))
+                    cls = int(cls)
+
+                    # rescale to origin
+                    c1 = (int(xmin * frame.shape[1] / detector.imgsz), int(ymin * frame.shape[0] / detector.imgsz))
+                    c2 = (int(xmax * frame.shape[1] / detector.imgsz), int(ymax * frame.shape[0] / detector.imgsz))
+
+                    cv2.rectangle(frame, c1, c2, detector.colors[cls],
+                                  thickness=line_thickness, lineType=cv2.LINE_AA)
+                    label = f'{detector.names[int(cls)]} {conf:.2f}'
+                    t_size = cv2.getTextSize(label, 0, fontScale=line_thickness / 3, thickness=font_thickness)[0]
+                    c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+                    cv2.rectangle(frame, c1, c2, detector.colors[cls], -1, cv2.LINE_AA)  # filled
+                    cv2.putText(frame, label, (c1[0], c1[1] - 2), 0, line_thickness / 3, [225, 255, 255], thickness=font_thickness,
+                                lineType=cv2.LINE_AA)
+
+        onDetected(result)
 
         display = cv2.resize(frame, (1280, 720))
         cv2.imshow(window_name, display)
--- Control_Simulator/yolo_detect.py
+++ Control_Simulator/yolo_detect.py
@@ -7,19 +7,89 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-# from models.experimental import attempt_load
-# from utils.datasets import LoadStreams, LoadImages
-# from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
-#     strip_optimizer, set_logging, increment_path
-# from utils.plots import plot_one_box
-# from utils.torch_utils import select_device, load_classifier, time_synchronized
+import numpy as np
 
+# Programmatically import yolov5 models
+# ==========================================
+#
+# import importlib
+# import sys
+# import os
+#
+# spec_models = importlib.util.spec_from_file_location("models", os.path.join(__yolov5_path, "models", "__init__.py"))
+# models = importlib.util.module_from_spec(spec_models)
+# sys.modules[spec_models.name] = models
+# spec_models.loader.exec_module(models)
+#
+# spec_utils = importlib.util.spec_from_file_location("utils", os.path.join(__yolov5_path, "utils", "__init__.py"))
+# utils = importlib.util.module_from_spec(spec_utils)
+# sys.modules[spec_utils.name] = utils
+# spec_utils.loader.exec_module(utils)
+#
+# import models
+# import utils
+# ==========================================
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+    strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 class YoloDetector:
 
-    def __init__(self):
-        print("hello")
+    def __init__(self, weights, device, img_size, conf_thres, iou_thres):
 
-    def Detect(self, img):
-        return 0
+        # Initialize
+        set_logging()
+        self.device = select_device(device)
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        # Load model
+        self.model = attempt_load(weights, map_location=self.device)  # load FP32 model
+        self.imgsz = check_img_size(img_size, s=self.model.stride.max())  # check img_size
+        if self.half:
+            self.model.half()  # to FP16
+
+        # Second-stage classifier
+        self.classify = False
+        if self.classify:
+            self.modelc = load_classifier(name='resnet101', n=2)  # initialize
+            self.modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=self.device)['model']).to(
+                self.device).eval()
+
+        # Get names and colors
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        # self.colors = [  # color in BGR
+        #     [255, 0, 0],
+        #     [0, 0, 255]
+        # ]
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+
+    def Detect(self, cv2_img):
+        img = cv2.resize(cv2_img, (self.imgsz, self.imgsz))
+        img = np.asarray(img)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        pred = self.model(img)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, None, None)
+
+        # Apply Classifier
+        if self.classify:
+            # pred = apply_classifier(pred, self.modelc, img, im0s)
+            raise NotImplementedError
+
+        return pred
 
