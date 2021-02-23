c6bcbe34f83bb0ac8ec6c466f3c05a50f2a412d4
NightOwl <midnightow1@outlook.com>
2021-02-24 00:43:47 +0800

    Add上位机运行录像保存

--- Robot/Main.cpp
+++ Robot/Main.cpp
@@ -11,9 +11,9 @@ using namespace std::filesystem;
 void PrintHelp()
 {
     cout << "SkiingRobot 0.2" << endl;
-    cout << "Usage: SkiingRobot <engine_file> <input>" << endl;
+    cout << "Usage: SkiingRobot <engine_file> <flag>" << endl;
     cout << "    engine_file: path to model TensorRT engine file" <<endl;
-    cout << "    input: path to input file or directory" <<endl;
+    cout << "    flag: 1=save run video" << endl;
     cout << endl;
 }
 
@@ -24,45 +24,74 @@ int PrintBadArguments()
     return 1;
 }
 
+std::string get_current_time_and_date()
+{
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d_%H-%M-%S");
+    return ss.str();
+}
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    if (argc < 2)
     {
         return PrintBadArguments();
     }
 
     Yolo5Engine engine = Yolo5Engine(argv[1], 224, 224);
+    VideoCapture capture(0);
+    capture.set(CAP_PROP_FRAME_WIDTH, 1280);
+    capture.set(CAP_PROP_FRAME_HEIGHT, 720);
 
-    if (is_directory(argv[2]))
+    bool flag_save_video = false;
+    VideoWriter writer;
+    if (argc >= 3 && strcmp("1", argv[2]) == 0)
     {
-        for (const auto& entry : directory_iterator(argv[2]))
+        flag_save_video = true;
+        std::stringstream filename;
+        filename << "run_" << get_current_time_and_date() << ".mp4";
+        writer = VideoWriter(filename.str(), VideoWriter::fourcc('M', 'P', '4', 'V'), 30, Size(1280, 720));
+    }
+
+    {
         {
-            auto start = std::chrono::system_clock::now();
-            string path = entry.path().string();
-            Mat img = imread(path);
-            auto result = engine.DoInfer(img, 0.3);
-            auto end = std::chrono::system_clock::now();
-            cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms : " << path << endl;
         }
     }
-    else if (is_regular_file(argv[2]))
+
+    Mat frame;
+    while (capture.read(frame))
     {
-        VideoCapture capture(argv[2]);
-        Mat frame;
-        while (capture.read(frame))
+        auto start = std::chrono::system_clock::now();
+        auto result = engine.DoInfer(frame, 0.3);
+        auto end = std::chrono::system_clock::now();
+        cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << endl;
+
+        auto info = onDetected(result, frame);
+
+        for (DetectedObject obj : result)
         {
-            auto start = std::chrono::system_clock::now();
-            auto result = engine.DoInfer(frame, 0.3);
-            auto end = std::chrono::system_clock::now();
-            cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << endl;
-
-            for (DetectedObject obj : result)
-            {
-                rectangle(frame, Point((float)obj.bbox.xMin, (float)obj.bbox.yMin),
-                          Point((float)obj.bbox.xMax, (float)obj.bbox.yMax),
-                          Scalar(0, 255, 0));
-            }
-
+            rectangle(frame, Point((float)obj.bbox.xMin, (float)obj.bbox.yMin),
+                      Point((float)obj.bbox.xMax, (float)obj.bbox.yMax),
+                      Scalar(0, 255, 0));
         }
+
+        if (flag_save_video)
+        {
+            writer.write(frame);
+        }
+#ifdef WIN32
+        imshow("Test", frame);
+        if ((waitKey(1) & 0xFF) == 'q')
+        {
+            break;
+        }
+#endif
+    }
+
+    if (flag_save_video)
+    {
+        writer.release();
     }
 }
